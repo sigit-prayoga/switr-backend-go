c5329aaa59e5bca9141e94e8d9ce48b2d2c73321
Sigit Prayoga <sigit.prayoga@obortech.com>
2016-10-24 11:15:39 +0700

    Fix mongodb session

--- main.go
+++ main.go
@@ -10,83 +10,75 @@ import (
 	"goji.io"
 	"goji.io/pat"
 	"golang.org/x/net/context"
-	"gopkg.in/mgo.v2"
+	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
-var session *mgo.Session
-
 func main() {
+	//trying to connect to mongo
+	db, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		//something happens when dialing
+		log.Fatal("Cannot dial mongo", err)
+	}
+	//clear when we're done
+	defer db.Close()
 	//initiate new multiplexer
 	mux := goji.NewMux()
-	mux.HandleFuncC(pat.Get("/swit/:switId"), getSwit)
-	mux.HandleFuncC(pat.Get("/swits"), getAllSwits)
+	//register all handler for each end point
+	mux.HandleFuncC(pat.Get("/swit/:switId"), getSwit(db))
+	mux.HandleFuncC(pat.Get("/swits"), getAllSwits(db))
+	//finally, listen and serve in designated host and port
 	http.ListenAndServe("localhost:3001", mux)
 }
 
-func getAllSwits(ctx context.Context, w http.ResponseWriter, req *http.Request) {
-	//prepare for the response
-	var swits []*model.Swit
-	//catch them all and if there is an error
-	err := getSession().DB("swit_app").C("swits").Find(nil).All(&swits)
-	if err != nil {
-		//let's get panic instead
-		panic(err)
-	}
-	// _ as blank identifier,the function returns multiple values, but we don't care
-	switsJSON, _ := json.Marshal(swits)
-	//write a response
-	ResponseWithJSON(w, switsJSON, http.StatusOK)
-}
-
-func createUser(ctx context.Context, w http.ResponseWriter, req *http.Request) {
-	decoder := json.NewDecoder(req.Body)
-	var user model.User
-	err := decoder.Decode(user)
-	if err != nil {
-		panic(err)
-	}
-
-	defer req.Body.Close()
-	log.Println(user.Name)
-}
-
-func getSession() *mgo.Session {
-	if session == nil {
-		log.Println("empty session")
-		var err error
-		session, err = mgo.Dial("localhost:27017")
-		//check error connection, is mongo running?
+func getAllSwits(s *mgo.Session) goji.HandlerFunc {
+	return func(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+		//create a copy of a session
+		session := s.Copy()
+		//clear the copied session once it's done
+		defer session.Close()
+		//prepare for the response
+		var swits []*model.Swit
+		//catch them all and if there is an error
+		err := session.DB("swit_app").C("swits").Find(nil).All(&swits)
 		if err != nil {
-			log.Fatal(err)
+			//let's get panic instead
 			panic(err)
 		}
+		// _ as blank identifier,the function returns multiple values, but we don't care
+		switsJSON, _ := json.Marshal(swits)
+		//write a response
+		ResponseWithJSON(w, switsJSON, http.StatusOK)
 	}
-
-	copiedSession := session.Copy()
-	defer copiedSession.Clone()
-
-	return copiedSession
 }
 
-func getSwit(ctx context.Context, w http.ResponseWriter, req *http.Request) {
-	switID := pat.Param(ctx, "switId")
-	fmt.Printf("swit id is %s \n", switID)
-	//get the db session
-	swits := getSession().DB("swit_app").C("swits")
-	var swit model.Swit
-	err := swits.Find(bson.M{"switId": switID}).One(&swit)
-	if err != nil {
-		panic(err)
-	}
+func getSwit(s *mgo.Session) goji.HandlerFunc {
+	return func(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+		//create a copy of a session
+		session := s.Copy()
+		//clear the copied session once it's done
+		defer session.Close()
+		//get query param out of the http request
+		switID := pat.Param(ctx, "switId")
+		fmt.Printf("swit id is %s \n", switID)
+		//get swits collection
+		c := session.DB("swit_app").C("swits")
+		var swit model.Swit
+		err := c.Find(bson.M{"switId": switID}).One(&swit)
+		if err != nil {
+			panic(err)
+		}
 
-	fmt.Printf("the swit object is : %s \n", swit)
-	respBody, _ := json.Marshal(swit)
-	ResponseWithJSON(w, respBody, http.StatusOK)
+		fmt.Printf("the swit object is : %s \n", swit)
+		//toJson
+		respBody, _ := json.Marshal(swit)
+		ResponseWithJSON(w, respBody, http.StatusOK)
+	}
 }
 
 func ResponseWithJSON(w http.ResponseWriter, json []byte, code int) {
-	fmt.Printf("Response Body : %q", json)
+	fmt.Println("Response Body : ", string(json))
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(code)
 	w.Write(json)
